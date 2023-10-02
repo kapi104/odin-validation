a66e7a8ffb75ac2e7ab2ec4da433480670ad1da8
kapi104 <skorupa7979@gmail.com>
2023-10-02 21:22:23 +0200

    formatted with prettier

--- src/index.js
+++ src/index.js
@@ -6,50 +6,50 @@ import './style.css';
 // const pass = document.querySelector('#password')
 // const rPass = document.querySelector('#rPassword')
 
-const inputs = [...document.querySelectorAll('input')]
+const inputs = [...document.querySelectorAll('input')];
 
-const checkedInputs = inputs.slice(0, -2)
+const checkedInputs = inputs.slice(0, -2);
 
 const showError = (input) => {
-  const spanTxt = input.nextElementSibling
-  const inputClasslist = input.classList
+  const spanTxt = input.nextElementSibling;
+  const inputClasslist = input.classList;
 
-  inputClasslist.add('error')
+  inputClasslist.add('error');
 
   if (input.validity.valueMissing) {
-    spanTxt.innerText = `Please input your ${input.name}`
+    spanTxt.innerText = `Please input your ${input.name}`;
   } else if (input.validity.typeMismatch) {
-    spanTxt.innerText = `entered value is not ${input.name}`
+    spanTxt.innerText = `entered value is not ${input.name}`;
   } else if (input.validity.patternMismatch) {
-    spanTxt.innerText = `entered value is not ${input.name}`
+    spanTxt.innerText = `entered value is not ${input.name}`;
   } else if (input.validity.tooShort) {
-    spanTxt.innerText = `${input.name} must be at least ${input.minLength} characters`
+    spanTxt.innerText = `${input.name} must be at least ${input.minLength} characters`;
   }
-}
+};
 
 const checkValidity = (i) => {
   if (i.validity.valid) {
-    const spanTxt = i.nextElementSibling
-    const inputClasslist = i.classList
+    const spanTxt = i.nextElementSibling;
+    const inputClasslist = i.classList;
 
-    inputClasslist.remove('error')
+    inputClasslist.remove('error');
 
     console.log(spanTxt);
-    spanTxt.innerText = ''
+    spanTxt.innerText = '';
   } else {
-    showError(i)
+    showError(i);
   }
-}
+};
 
 checkedInputs.forEach((i) => {
-  i.addEventListener('input', () => checkValidity(i))
-  i.addEventListener('focus', () => checkValidity(i))
+  i.addEventListener('input', () => checkValidity(i));
+  i.addEventListener('focus', () => checkValidity(i));
 });
 
-const disabledValidationMessage = inputs.slice(0, -1)
+const disabledValidationMessage = inputs.slice(0, -1);
 
-disabledValidationMessage.forEach(i => {
+disabledValidationMessage.forEach((i) => {
   i.addEventListener('invalid', (e) => {
-    e.preventDefault()
-  })
-})
+    e.preventDefault();
+  });
+});
